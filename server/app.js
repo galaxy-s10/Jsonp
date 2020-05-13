d11da37afb2ba7d9df4bfeb3eab418754b116f46
galaxy-s10 <2274751790@qq.com>
2020-05-13 16:53:02 +0800

    封装jsonp

--- server/app.js
+++ server/app.js
@@ -2,10 +2,41 @@ var express = require('express')
 var url = require('url')
 var app = express()
 
+
+app.use('/login', function (req, res) {
+    var urlParse = url.parse(req.url, true)
+    console.log(urlParse);
+    var cb = urlParse.query.callback
+    var name = urlParse.query.name
+    var pwd = urlParse.query.pwd
+    if (name == 'admin' && pwd == 123456) {
+        const info = { name: '张三', age: 19 }
+        console.log(JSON.stringify(info));
+        var jsondata = cb + "('" + JSON.stringify(info) + "')"
+        console.log(jsondata);
+        // setTimeout(() => {
+        res.end(jsondata)
+        // }, 2000);
+    }
+
+})
+app.use('/ajax', function (req, res) {
+    var urlParse = url.parse(req.url, true)
+    console.log(urlParse);
+    var cb = urlParse.query.cbName
+    // setTimeout(() => {
+    res.end(cb + "('我是ajaxjsonp返回的数据')")
+    // }, 2000);
+
+})
 app.use('/', function (req, res) {
     var urlParse = url.parse(req.url, true)
+    console.log(urlParse);
     var cb = urlParse.query.callback
-    res.end(cb + "('I am jsonp data')")
+    // setTimeout(() => {
+    res.end(cb + "('我是原生jsonp返回的数据')")
+    // }, 3000);
+
 })
 
 app.listen('3000', function () {
